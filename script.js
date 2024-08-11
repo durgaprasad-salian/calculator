334d6d4cf1e74bc16cf101febb16b4ad6fc847cf
Durgaprasad <167079075+durgaprasad-salian@users.noreply.github.com>
2024-08-11 13:59:37 +0530

    Adding CSS styles to the calculator

--- script.js
+++ script.js
@@ -16,3 +16,38 @@ function divide(a, b){
 
     return a/b ;
 }
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let displayValue = "";
+
+function operate(operator, firstNumber, secondNumber){
+
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+            
+        case "-":
+            return subtract(firstNumber, secondNumber);
+
+        case "*":
+            return multiply(firstNumber, secondNumber);
+
+        case "/":
+            return divide(firstNumber, secondNumber);
+
+        default:
+            return "Please enter a valid operation"
+
+    }
+}
+
+
+/* let a = Number(window.prompt("Enter a"));
+let op = (window.prompt("Enter operator"));
+let b = Number(window.prompt("Enter b"));
+
+let c = operate(op, a, b);
+
+alert(c); */
