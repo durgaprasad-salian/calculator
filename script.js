618efa7aef51440b0b56e2661bf266723f0cf915
Durgaprasad <167079075+durgaprasad-salian@users.noreply.github.com>
2024-08-14 23:56:03 +0530

    Commit 4: #Adding event listener for mouse clicks #Handling all operations #Few cosmatic changes

--- script.js
+++ script.js
@@ -17,24 +17,25 @@ function divide(a, b){
     return a/b ;
 }
 
-let firstNumber = null;
-let secondNumber = null;
+let firstNumber = "";
+let secondNumber = "";
 let operator = null;
 let displayValue = "";
+let operationSelected = false;
 
 function operate(operator, firstNumber, secondNumber){
 
     switch (operator) {
-        case "+":
+        case '+':
             return add(firstNumber, secondNumber);
             
-        case "-":
+        case '-':
             return subtract(firstNumber, secondNumber);
 
-        case "*":
+        case 'x':
             return multiply(firstNumber, secondNumber);
 
-        case "/":
+        case '÷':
             return divide(firstNumber, secondNumber);
 
         default:
@@ -43,11 +44,166 @@ function operate(operator, firstNumber, secondNumber){
     }
 }
 
+function truncateTo15Digits(value) {
 
-/* let a = Number(window.prompt("Enter a"));
-let op = (window.prompt("Enter operator"));
-let b = Number(window.prompt("Enter b"));
+    let numberStr = String(value);
 
-let c = operate(op, a, b);
+    if(numberStr.length > 15) {
 
-alert(c); */
+        if(numberStr.includes(".")) {
+            const [integerPart, decimalPart] = numberStr.split(".");
+            const totalDigits = integerPart.length;
+            const remainingDigits = 15 - totalDigits;
+
+            if(remainingDigits > 0) {
+                return (integerPart + '.' + decimalPart.substring(0, remainingDigits));
+            } else {
+                return integerPart;
+            }
+
+        } else {
+            return "TOO LARGE TO DISPLAY";
+        }
+    } else {
+        return numberStr;
+    }
+}
+
+function updateDisplay(value) {
+
+    value = truncateTo15Digits(value);
+
+    let operationDisplay = document.querySelector('#operation');
+    let currentValueDisplay = document.querySelector('#currentValue');
+
+    if ( firstNumber !== "" && secondNumber !== "") {
+
+        operationDisplay.innerText = `${firstNumber} ${operator} ${secondNumber}`;
+
+    } else if(firstNumber !== "" && operator !== "") {
+
+        operationDisplay.innerText = `${firstNumber} ${operator}`;
+
+    } else {
+        operationDisplay.innerText = '';
+    }
+
+    currentValueDisplay.innerText = value || '0';
+}
+
+function clearCalculator() {
+    
+    firstNumber = "";
+    secondNumber = "";
+    operator = null;
+    displayValue = "";
+    operationSelected = false;
+    updateDisplay(displayValue);
+
+}
+
+function handleNumberClick(value) {
+
+    if(operationSelected) {
+
+        displayValue = secondNumber;
+        
+        displayValue += value;
+       
+        secondNumber = displayValue;
+
+        console.log(`secondNumber value : ${secondNumber}`)
+
+    } else {
+
+        displayValue += value; //this is to accommodate number which are greater than 9
+    
+    } 
+
+    updateDisplay(displayValue);
+}
+
+function handleOperatorClick(value) {
+
+    if (operationSelected) {
+        
+        handleEqualsClick();
+    }
+
+    firstNumber = displayValue;
+    operator = value;
+    operationSelected = true;
+}
+
+function handleEqualsClick() {
+    
+    if ( firstNumber !== "" && secondNumber !== "") {
+
+        let result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+        displayValue = result;
+        console.log(`result : ${result}`);
+        updateDisplay(displayValue);
+
+        operationSelected = false;
+        secondNumber = "";
+    }
+    
+}
+
+function handleDotClick() {
+
+    if(!displayValue.includes("."))
+    {
+        if(operationSelected) {
+
+            secondNumber = displayValue + ".";
+    
+        } 
+            displayValue = displayValue + "." ;
+        
+    
+        updateDisplay(displayValue);
+    }
+  
+}
+
+function handleDeleteClick(){
+    
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay(displayValue);
+}
+
+document.querySelectorAll('.buttonsContainer button').forEach((button) => {
+    
+    button.addEventListener('click', (event) => {
+        
+        let value = event.target.innerText;
+        console.log(`${value} button got clicked`);
+
+        if (!isNaN(value)) { // Checking button if the button is number
+
+            handleNumberClick(value);
+        
+        } else if (value === 'Clear') {
+
+            clearCalculator();
+        
+        } else if (value === 'Delete') {
+
+            handleDeleteClick();
+
+        } else if (value === '=') {
+
+            handleEqualsClick();
+
+        } else if (value === '.') {
+
+            handleDotClick();
+        
+        } else { //Here button will be any of the 4 operator
+
+            handleOperatorClick(value);
+        }
+
+    });
+});
